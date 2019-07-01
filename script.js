1d639ef5731b4768bc78439fad797296eb58fdc1
Lia <lia.tabilo@gmail.com>
2019-07-01 12:57:11 +0100

    Add weather details function

--- script.js
+++ script.js
@@ -5,19 +5,28 @@ let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${a
 function showTemp(response) {
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = `${Math.round(response.data.main.temp)}`;
+
+  let place = document.querySelector("#place");
+  place.innerHTML = response.data.name;
 }
 
 axios.get(apiUrl).then(showTemp);
 
-function showHumidity(response) {
+function weatherDetails(response) {
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-}
-axios.get(apiUrl).then(showHumidity);
 
-function showWind(response) {
   let windSpeed = document.querySelector("#windSpeed");
   windSpeed.innerHTML = `Wind Speed: ${response.data.wind.speed}hm/h`;
-}
 
-axios.get(apiUrl).then(showWind);
+  let description = document.querySelector("#description");
+  description.innerHTML = `Sky: ${response.data.weather[0].description}`;
+}
+axios.get(apiUrl).then(weatherDetails);
+
+let now = new Date();
+let month = now.getMonth();
+let day = now.getDate();
+
+let time = document.querySelector("#time");
+time.innerHTML = now;
