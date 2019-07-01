7948c94ba11efe6dd3462514a6227446a5fa5baf
Lia <lia.tabilo@gmail.com>
2019-07-01 15:32:36 +0100

    Add image icon

--- script.js
+++ script.js
@@ -2,7 +2,50 @@ let city = "Lisbon";
 let apiKey = "2e84a4ecaf1708e1643c02326f19599e";
 let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-function showTemp(response) {
+function showDate(date) {
+  let year = date.getFullYear();
+  let dayNumber = date.getDate();
+  let hour = date.getHour();
+
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let daysName = days[day];
+
+  let month = date.getMonth();
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Nov",
+    "Dec"
+  ];
+
+  let monthsName = months[month];
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${daysName} ${monthsName} ${dayNumber}, ${year} ${hour}:${minute}`;
+}
+
+function displayWeather(response) {
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = `${Math.round(response.data.main.temp)}`;
 
@@ -22,46 +65,12 @@ function showTemp(response) {
 
   let date = document.querySelector("#date");
   date.innerHTML = showDate(new Date(response.data.dt * 1000));
-  console.log(response.data);
+
+  let icon = document.querySelector("#icon");
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/w/${response.data.weather[0].icon}.png`
+  );
 }
-axios.get(apiUrl).then(showTemp);
 
-function showDate(date) {
-  let hour = date.getHours();
-  let dayNumber = date.getDate();
-
-  let dayName = date.getDay();
-  let daysName = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-
-  let month = date.getMonth();
-  let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Nov",
-    "Dec"
-  ];
-
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  return `${daysName(dayName)} ${months(
-    month
-  )} ${dayNumber}, ${hour}:${minute}`;
-}
+axios.get(apiUrl).then(displayWeather);
