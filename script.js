c826f644e46edeaed4684f0864511ea4a95335c4
Lia <lia.tabilo@gmail.com>
2019-07-02 09:49:08 +0100

    Create checkCity event

--- script.js
+++ script.js
@@ -2,49 +2,49 @@ let city = "Lisbon";
 let apiKey = "2e84a4ecaf1708e1643c02326f19599e";
 let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-function showDate(date) {
-  let day = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
+// function showDate(date) {
+//   let day = date.getDay();
+//   let days = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday"
+//   ];
 
-  let daysName = days[day];
+//   let daysName = days[day];
 
-  let month = date.getMonth();
-  let months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Nov",
-    "Dec"
-  ];
+//   let month = date.getMonth();
+//   let months = [
+//     "Jan",
+//     "Feb",
+//     "Mar",
+//     "Apr",
+//     "May",
+//     "Jun",
+//     "Jul",
+//     "Aug",
+//     "Sep",
+//     "Nov",
+//     "Dec"
+//   ];
 
-  let monthsName = months[month];
+//   let monthsName = months[month];
 
-  let dayNumber = date.getDate();
-  let hour = date.getHours();
+//   let dayNumber = date.getDate();
+//   let hour = date.getHours();
 
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
+//   let minutes = date.getMinutes();
+//   if (minutes < 10) {
+//     minutes = `0${minutes}`;
+//   }
 
-  return `${daysName} ${monthsName} ${dayNumber}, ${hour}:${minute}`;
-}
+//   return `${daysName} ${monthsName} ${dayNumber}, ${hour}:${minute}`;
+// }
 
-function checkCity(response) {
+function checkCity(event) {
   event.preventDefault();
   let keyword = document.querySelector("#keyword");
   alert(keyword.value);
@@ -68,8 +68,8 @@ function displayWeather(response) {
     response.data.wind.speed
   )} km/h`;
 
-  let date = document.querySelector("#date");
-  date.innerHTML = showDate(new Date(response.data.dt * 1000));
+  //let date = document.querySelector("#date");
+  //date.innerHTML = showDate(new Date(response.data.dt * 1000));
 
   let icon = document.querySelector("#icon");
   icon.setAttribute(
